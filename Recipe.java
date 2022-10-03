e1028308cd29794870dc7071c0455b6a8672ea54
Anh Tran <101434320+Anhtrr@users.noreply.github.com>
2022-10-03 18:07:14 -0400

    Update Recipe.java
    added constructor for recipe(), added getters and setters.

--- Recipe.java
+++ Recipe.java
@@ -1,14 +1,54 @@
-public class Recipe {
-
-
-	private String[] ingredientslist = new String[]
-
-
-	private Name; 
-
-	private instructionList;
-
-	//add getters and setters
-
-
+public class Recipe{
+	
+	// Name of recipe as a string
+	private String name; 
+	
+	// Ingredients stored in an arraylist of strings 
+	private String[] ingredientsList;
+	
+	// store instructions as a long string separated by new line
+	private String instructionList;
+	
+	// constructor for recipe
+	public Recipe(String name, String[] ingredientsList, String instructionList) {
+		this.name = name;
+		this.ingredientsList = ingredientsList;
+		this.instructionList = instructionList;
+	}
+	
+	// getter for name 
+	public String getName() {
+		return Name;
+	}
+	
+	// setter for name
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	// getter for ingredientsList
+	public String[] getIngredientsList() {
+		return ingredientsList;
+	}
+	
+	// setter for ingredientsList
+	public void setIngredientsList(String[] ingredientsList) {
+		for (int i = 0; i < ingredientsList.length(), i++) {
+			this.ingredientsList[i] = ingredientsList[i];
+		}
+	}
+	
+	// getter for instructionList
+	public String getInstructionList() {
+		return instructionList;
+	}
+	
+	// setter for instructionList
+	public void setInstructionList(String instructionList) {
+		this.instructionList = instructionList;
+	}
+	
 }
+
+// add getters and setters
+
